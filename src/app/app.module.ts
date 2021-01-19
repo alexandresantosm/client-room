57ed73da65922c6626b143d6ecdd303d00406fa0
alexandresantosm <amenezes031@gmail.com>
2021-01-18 21:40:32 -0300

    Create component room details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateRoomComponent
+    CreateRoomComponent,
+    RoomDetailsComponent
   ],
   imports: [
     BrowserModule,
