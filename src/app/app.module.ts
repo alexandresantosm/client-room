4a35a3e828285e5e617773ebc762a55fa033115b
alexandresantosm <amenezes031@gmail.com>
2021-01-18 21:39:17 -0300

    Create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent
   ],
   imports: [
     BrowserModule,
