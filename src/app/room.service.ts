1abcb605fbabc50078647e1abe3a246d6feb07a7
alexandresantosm <amenezes031@gmail.com>
2021-01-19 18:41:28 -0300

    Backend connection configuration

--- src/app/room.service.ts
+++ src/app/room.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
+  private baseUrl= 'http://localhost:8080/api/v1/rooms';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getRoomList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+  getRoom(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createRoom(room: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, room);
+  }
+
+  updateRoom(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
+  }
+
+  deleteRoom(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, {responseType: 'text'});
+  }
 }
