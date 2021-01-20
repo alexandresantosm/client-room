e023847091829a02f2af7b0331d997925e1a6a43
alexandresantosm <amenezes031@gmail.com>
2021-01-19 22:04:07 -0300

    Definition component room-details

--- src/app/room-details/room-details.component.ts
+++ src/app/room-details/room-details.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { RoomService} from '../room.service';
+import { Room } from '../room';
+import { RoomListComponent } from '../room-list/room-list.component';
 
 @Component({
   selector: 'app-room-details',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-details.component.css']
 })
 export class RoomDetailsComponent implements OnInit {
+  id: number;
+  room: Room;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private roomService: RoomService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.room = new Room();
+
+    this.id = this.route.snapshot.params['id'];
+
+    this.roomService.getRoom(this.id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.room = data;
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+
+  list() {
+    this.router.navigate(['rooms']);
   }
 
 }
