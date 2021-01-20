08f191bcea338b55c21cd0fb3249726183e87803
alexandresantosm <amenezes031@gmail.com>
2021-01-19 21:45:36 -0300

    Definition component room-list

--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { Room } from '../room';
+import { RoomService } from '../room.service';
+import { RoomDetailsComponent } from '../room-details/room-details.component';
 
 @Component({
   selector: 'app-room-list',
@@ -6,10 +12,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-list.component.css']
 })
 export class RoomListComponent implements OnInit {
+  rooms: Observable<Room[]>;
 
-  constructor() { }
+  constructor(
+    private roomService: RoomService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.reloadData();
   }
 
+  reloadData() {
+    this.rooms = this.roomService.getRoomList();
+  }
+
+  roomDetails(id: number) {
+    this.router.navigate(['details', id]);
+  }
+
+  updateRoom(id: number) {
+    this.router.navigate(['update', id]);
+  }
+
+  deleteRoom(id: number) {
+    this.roomService.deleteRoom(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log(error)
+      );
+  }
 }
